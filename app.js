a3cbc027f88f50fd93fe52ee25b3826f5b6c5d98
neiderc <cneider54@gmail.com>
2025-05-11 14:12:29 -0500

    agrego libreria y comentario para iniciar el proyecto

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');

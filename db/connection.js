d349917a3980045775b362a26f9af2442ce85525
YairCordoba <Yaocoepa654@gmail.com>
2025-05-12 16:39:15 -0500

    Actualiza enlaces de iconos y agrega sección de reportes en varias páginas

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const connect = mysql.createConnection({
     database: 'ing_soft',
-    user: 'pauzubuoscv5bj2zy70t',
+    user: 'i8dug1f8ojbreopdjpv5',
     host: 'aws.connect.psdb.cloud',
-    password: 'pscale_pw_44ai9DLJbUuA5mE4phofbtOD0v8abLQaaNlD0pVS0wO',
+    password: 'pscale_pw_9adxPInZ9YG2XFpgmJYSewqy5IpZzoAdhbovgfksdV2',
     ssl: { rejectUnauthorized: false }
 });
 
